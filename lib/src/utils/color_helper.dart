e9301946a40b4f96159391062b099cd4823d7cf7
Valor Zhong <valorzhong@gmail.com>
2014-04-28 18:44:32 +0800

    alpha set default value

--- lib/src/utils/color_helper.dart
+++ lib/src/utils/color_helper.dart
@@ -4,7 +4,7 @@ part of valorzhong_bones;
 
 class ColorHelper {
   
-  static int fromRgba(int red, int green, int blue, double alpha) {
+  static int fromRgba(int red, int green, int blue, [double alpha = 1.0]) {
     var ai = Math.min(1, Math.max(alpha, 0));
     return (ai * 255).toInt() << 24 | red << 16 | green << 8 | blue;
   }
