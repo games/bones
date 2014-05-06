69130536bf567e530e4773f40b066d827d42d7ad
Valor Zhong <valorzhong@gmail.com>
2014-05-06 18:41:12 +0800

    list view

--- lib/src/controls/list_view.dart
+++ lib/src/controls/list_view.dart
@@ -1,17 +1,62 @@
 part of valorzhong_bones;
 
 
-
+typedef DisplayObject ItemRenderer(num position, data);
 
 class ListView extends Skinnable {
   DisplayObject header;
   DisplayObject footer;
-  DisplayObject divider;
+  num itemWidth, itemHeight;
+  ItemRenderer itemRenderer;
+  ItemRenderer dividerRenderer;
+  List _data;
 
-  ListView(Skin skin): super(skin);
+  ListView([List data, Skin skin])
+      : _data = data,
+        super(skin);
 
+  List get data => _data;
 
+  void set data(val) {
+    _data = val;
+    invalidate();
+  }
 
   @override
-  Skin get defaultSkin => null;
+  repaint() {
+    super.repaint();
+    removeChildren();
+    if (_data != null && itemRenderer != null) {
+      var num = _data.length,
+          bottom = 0;
+      if (header != null) {
+        addChild(header
+            ..x = 0
+            ..y = 0);
+        bottom = header.height;
+      }
+      for (var i = 0; i < num; i++) {
+        var item = itemRenderer(i, _data[i])
+            ..x = 0
+            ..y = bottom;
+        bottom += item.height;
+        addChild(item);
+        if (dividerRenderer != null) {
+          var divider = dividerRenderer(i, _data[i])
+              ..x = 0
+              ..y = bottom;
+          bottom += divider.height;
+          addChild(divider);
+        }
+      }
+      if (footer != null) {
+        addChild(footer
+            ..x = 0
+            ..y = bottom);
+      }
+    }
+  }
+
+  @override
+  Skin get defaultSkin => new ListViewSkin();
 }
--- lib/src/skins/list_view_skin.dart
+++ lib/src/skins/list_view_skin.dart
@@ -1,2 +1,69 @@
 part of valorzhong_bones;
 
+
+
+
+class ListViewSkin extends Skin {
+
+  ItemRenderer renderer;
+
+  ListViewSkin({this.renderer, num width, num height}): super() {
+    this.width = width;
+    this.height = height;
+  }
+
+  @override
+  apply() {
+    var listView = target as ListView;
+    listView.itemWidth = width == null ? 200 : width;
+    listView.itemHeight = height == null ? 50 : height;
+    listView.itemRenderer = renderer != null ? renderer : (num position, item) {
+      var btn = new Button(align: HorizontalAlign.LEFT, padding: 10, skin: new ListViewItemSkin(width: listView.itemWidth, height: listView.itemHeight));
+      if (item is Map) {
+        if (item.containsKey("icon")) {
+          btn.icon = item["icon"];
+        }
+        if (item.containsKey("label")) {
+          btn.text = item["label"];
+        }
+      } else {
+        btn.text = item.toString();
+      }
+      return btn;
+    };
+  }
+}
+
+class ListViewItemSkin extends Skin {
+
+  ListViewItemSkin({num width: 200, num height: 50})
+      : super() {
+    this.width = width;
+    this.height = height;
+  }
+
+  @override
+  apply() {
+    var upState = new Shape()
+        ..graphics.beginPath()
+        ..graphics.rect(0, 0, width, height)
+        ..graphics.fillColor(Color.White)
+        ..graphics.strokeColor(Color.Gray);
+    var overState = new Shape()
+        ..graphics.beginPath()
+        ..graphics.rect(0, 0, width, height)
+        ..graphics.fillColor(Color.WhiteSmoke)
+        ..graphics.strokeColor(Color.Gray);
+    var downState = new Shape()
+        ..graphics.beginPath()
+        ..graphics.rect(0, 0, width, height)
+        ..graphics.fillColor(Color.WhiteSmoke)
+        ..graphics.strokeColor(Color.Gray, 2);
+    var hitTestState = upState;
+    (target as Button)
+        ..upState = upState
+        ..overState = overState
+        ..downState = downState
+        ..hitTestState = upState;
+  }
+}
