64ac0814f6b5f14354f1b96baefaa73d753deb74
Valor Zhong <valorzhong@gmail.com>
2014-05-12 17:43:24 +0800

    refactoring alert

--- lib/src/controls/alert.dart
+++ lib/src/controls/alert.dart
@@ -10,9 +10,78 @@ class Alert extends Skinnable {
   bool isModal;
   bool cover;
   num bodyWidth, bodyHeight;
-  List<ButtonDef> buttonDefs;
 
-  Alert(this.message, {this.title: null, this.isModal: true, this.cover: true, this.buttonDefs, this.bodyWidth: null, this.bodyHeight: null, Skin skin: null}): super(skin);
+  num padding = 20;
+  DisplayObject background;
+  ButtonGroup buttons;
+  DisplayObject header;
+  DisplayObject content;
+
+  @override
+  repaint() {
+    super.repaint();
+
+    var doublePadding = padding * 2;
+
+    removeChildren();
+    addChild(background);
+
+    if (bodyWidth != null && bodyHeight != null) {
+      _width = bodyWidth + doublePadding;
+      _height = bodyHeight + doublePadding;
+      content
+          ..width = bodyWidth
+          ..height = bodyHeight;
+    } else if (content != null) {
+      _width = content.width + doublePadding;
+      _height = content.height + doublePadding;
+    } else {
+      _width = doublePadding;
+      _height = doublePadding;
+    }
+
+    var headerHeight = 0;
+    if (header != null) {
+      header
+          ..x = padding
+          ..y = padding;
+      addChild(header);
+      headerHeight = header.height + doublePadding;
+      _width = Math.max(header.width + doublePadding, _width);
+      _height = _height + header.height + doublePadding;
+    }
+
+    if (content != null) {
+      content
+          ..width = _width - doublePadding
+          ..x = padding
+          ..y = headerHeight + (_height - headerHeight - content.height) / 2;
+      addChild(content);
+    }
+
+    if (buttons != null) {
+      _width = Math.max(_width, buttons.width + doublePadding);
+      _height = _height + doublePadding + buttons.height;
+      buttons.x = (_width - buttons.width) / 2;
+      buttons.y = _height - buttons.height - doublePadding;
+      addChild(buttons);
+    }
+
+    background
+        ..width = _width.toInt()
+        ..height = _height.toInt();
+  }
+
+
+  Alert(this.message, {this.title: null, this.isModal: true, this.cover: true, List<ButtonDef> buttonDefs, this.bodyWidth: null, this.bodyHeight: null, Skin skin: null}): super(skin) {
+    if (buttonDefs != null) {
+      buttons = new ButtonGroup(buttonDefs);
+      buttons.onSelected.listen((e) {
+        dispatchEvent(new Event(e.event));
+        close();
+      });
+    }
+  }
 
   close() => dispatchEvent(new Event(Event.CLOSE));
 
@@ -27,9 +96,6 @@ class Alert extends Skinnable {
   static Alert okayCancel(String message, {String title: null, bool isModal: true, bool cover: true, Skin skin: null, bodyWidth: null, bodyHeight: null}) {
     return new Alert(message, title: title, isModal: isModal, cover: cover, buttonDefs: BUTTONS_OK_CANCEL, skin: skin, bodyWidth: bodyWidth, bodyHeight: bodyHeight);
   }
-
-  @override
-  Skin get defaultSkin => new AlertSkin();
 }
 
 class PopupManager {
--- lib/src/skins/default/alert_skin.dart
+++ lib/src/skins/default/alert_skin.dart
@@ -2,114 +2,32 @@ part of bones;
 
 
 class AlertSkin extends Skin {
-  num padding, doublePadding;
-
-  AlertSkin({this.padding: 8}): super() {
-    doublePadding = padding * 2;
-  }
 
   @override
   apply() {
-
+    var alert = target as Alert;
+    if (alert.title != null) {
+      alert.header = new TextField()
+          ..defaultTextFormat.size = 12
+          ..defaultTextFormat.bold = true
+          ..autoSize = TextFieldAutoSize.CENTER
+          ..text = alert.title;
+    }
+    if (alert.message != null) {
+      alert.content = new TextField()
+          ..defaultTextFormat.size = 12
+          ..defaultTextFormat.align = TextFormatAlign.CENTER
+          ..multiline = true
+          ..wordWrap = true
+          ..autoSize = TextFieldAutoSize.CENTER
+          ..text = alert.message;
+    }
+    alert.background = createScale9Bitmap(new Shape()
+        ..graphics.rectRound(0, 0, 20, 20, 5, 5)
+        ..graphics.fillColor(Color.White), new Rectangle(5, 5, 5, 5));
   }
 
   @override
   repaint() {
-    var alert = target as Alert;
-    alert.removeChildren();
-
-    var background = new Shape();
-    alert.addChild(background);
-
-    TextField message;
-    if (alert.message != null) {
-      message = new TextField()
-        ..defaultTextFormat.size = 12
-        ..defaultTextFormat.align = TextFormatAlign.CENTER
-        ..autoSize = TextFieldAutoSize.CENTER
-        ..text = alert.message;
-    }
-    Rectangle bodyBounds;
-    if (alert.bodyWidth != null && alert.bodyHeight != null) {
-      bodyBounds = new Rectangle(0, 0, alert.bodyWidth + doublePadding, alert.bodyHeight + doublePadding);
-      message
-        ..multiline = true
-        ..wordWrap = true
-        ..width = alert.bodyWidth
-        ..height = alert.bodyHeight;
-    } else if (message != null) {
-      bodyBounds = new Rectangle(0, 0, message.width + doublePadding, message.height + doublePadding);
-    } else {
-      bodyBounds = new Rectangle(0, 0, doublePadding, doublePadding);
-    }
-
-    TextField title;
-    if (alert.title != null) {
-      title = new TextField()
-        ..defaultTextFormat.size = 12
-        ..defaultTextFormat.bold = true
-        ..autoSize = TextFieldAutoSize.CENTER
-        ..text = alert.title;
-    }
-
-    ButtonGroup buttons;
-    if (alert.buttonDefs != null) {
-      buttons = new ButtonGroup(alert.buttonDefs);
-      bodyBounds.width = Math.max(bodyBounds.width, buttons.width + doublePadding);
-      buttons.x = (bodyBounds.width - buttons.width) / 2;
-      buttons.y = bodyBounds.bottom + padding;
-      buttons.onSelected.listen((e) {
-        alert.dispatchEvent(new Event(e.event));
-        alert.close();
-      });
-      bodyBounds.height = bodyBounds.height + doublePadding + buttons.height;
-    }
-
-    Rectangle titleBounds;
-    if (title != null) {
-      titleBounds = new Rectangle(0, 0, Math.max(title.width + doublePadding, bodyBounds.width), title.height + doublePadding);
-      bodyBounds.top = titleBounds.top + titleBounds.height;
-      bodyBounds.width = titleBounds.width;
-    }
-
-    if (message != null) {
-      message
-        ..width = bodyBounds.width - doublePadding
-        ..x = bodyBounds.left + padding
-        ..y = bodyBounds.top + (bodyBounds.height - (buttons != null ? buttons.height : 0) - message.height) / 2;
-    }
-
-    if (buttons != null) {
-      buttons.x = (bodyBounds.width - buttons.width) / 2;
-      buttons.y = bodyBounds.bottom - buttons.height - padding;
-    }
-
-    background.graphics.clear();
-    if (titleBounds != null) {
-      background.graphics
-        ..beginPath()
-        ..rect(titleBounds.left, titleBounds.top, titleBounds.width, titleBounds.height)
-        ..fillColor(ColorHelper.fromRgba(222, 222, 222));
-      title
-        ..x = titleBounds.left + padding
-        ..y = titleBounds.top + padding;
-      alert.addChild(title);
-    }
-    background.graphics
-      ..beginPath()
-      ..rect(bodyBounds.left, bodyBounds.top, bodyBounds.width, bodyBounds.height)
-      ..fillColor(Color.White);
-    background.graphics
-      ..beginPath()
-      ..rect(0, 0, background.width, background.height)
-      ..strokeColor(Color.Black);
-
-    if (message != null) {
-      alert.addChild(message);
-    }
-    if (buttons != null) alert.addChild(buttons);
-    alert.measure();
-
-    background.applyCache(0, 0, background.width.toInt(), background.height.toInt());
   }
 }
